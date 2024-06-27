0b520c3787398a0ffc404ee7f0c3e159ec81beb9
debs-07 <debakantasatapathy07@gmail.com>
2024-06-27 22:51:12 +0530

    Clear history functionality and new icon

--- src/components/ListingPage/Header.tsx
+++ src/components/ListingPage/Header.tsx
@@ -1,9 +1,40 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { useDispatch } from "react-redux";
+import { clearDataFromChromeStore } from "../../store/slices/moviesSlice";
 
-const Header = () => (
-  <Typography variant="h4" sx={{ display: "flex", justifyContent: "center" }}>
-    Movie Lens
-  </Typography>
-);
+const Header = () => {
+  const dispatch = useDispatch();
+
+  const clearHistoryHandler = () => {
+    dispatch(clearDataFromChromeStore());
+    chrome.storage.local.clear();
+  };
+
+  return (
+    <>
+      <Typography
+        variant="h4"
+        sx={{ display: "flex", justifyContent: "center" }}
+      >
+        Movie Lens
+      </Typography>
+      <Button
+        size="small"
+        variant="contained"
+        startIcon={<DeleteOutlinedIcon />}
+        sx={{
+          textTransform: "none",
+          backgroundColor: "black",
+          color: "white",
+          marginBottom: "10px",
+        }}
+        onClick={() => clearHistoryHandler()}
+      >
+        Clear History
+      </Button>
+    </>
+  );
+};
 
 export default Header;
